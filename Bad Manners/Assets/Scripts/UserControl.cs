1958409adb3867367eab782bbcfcf9c434484bf0
Anton Klava <anton@klava.se>
2015-04-19 16:03:34 +0200

    Player now looks in the direction of movement

--- Bad Manners/Assets/Scripts/UserControl.cs
+++ Bad Manners/Assets/Scripts/UserControl.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class UserControl : MonoBehaviour {
 
-	private Rigidbody2D rb;
 	public float velocityMultiplier = 5.0f;
+	public float angle = 0f;
+
+	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +18,8 @@ public class UserControl : MonoBehaviour {
 			Input.GetAxis("Horizontal"),
 			Input.GetAxis("Vertical")
 		).normalized * velocityMultiplier;
+		float atan2 = Mathf.Atan2(rb.velocity.y, rb.velocity.x) *  Mathf.Rad2Deg;
+		atan2 += angle;
+		transform.rotation = Quaternion.AngleAxis(atan2, Vector3.forward);
 	}
 }
