988a9c72313a314da3482dbe667f332ad73d4731
Anton Lejon <little.burrito@gmail.com>
2015-04-19 10:41:55 +0200

    Mer interface i personal office och mobil

---
+++ Bad Manners/Assets/Scripts/Mobile.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Mobile : MonoBehaviour {
+
+    public Text visibleMessage;
+    private float previousX;
+    private float previousY;
+    private float previousZ;
+    private float newX;
+    private float newY;
+    public float animationDuration;
+    private float newWaypointTime;
+    public bool isActive;
+    public AudioSource newMessageSound;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        isActive = false;
+        anim = GetComponent<Animator>();
+        SetText( "Hej på dig!" );
+	}
+
+    public void SetText( string text ) {
+        visibleMessage.text = text;
+        newMessageSound.Play();
+    }
+
+    public void Toggle() {
+        if ( isActive ) {
+            Deactivate();
+        } else {
+            Activate();
+        }
+    }
+
+    private void UpdateAnimator() {
+        anim.SetBool( "isActive", isActive );
+    }
+
+    public void Activate() {
+        isActive = true;
+        UpdateAnimator();
+    }
+
+    public void Deactivate() {
+        isActive = false;
+        UpdateAnimator();
+    }
+}
---
+++ Bad Manners/Assets/Scripts/Personal office/Clickables/ClickToChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToChangeScene : Clickable {
+
+    public ChangeScene changeSceneScript;
+
+    protected override void Action() {
+        changeSceneScript.Trigger();
+    }
+}
