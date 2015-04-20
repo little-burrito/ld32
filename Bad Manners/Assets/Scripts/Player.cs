72ecf34d078e5390cd5d44ce77da433b743e27cd
Anton Lejon <little.burrito@gmail.com>
2015-04-21 00:50:12 +0200

    SMS-ljud

--- Bad Manners/Assets/Scripts/Player.cs
+++ Bad Manners/Assets/Scripts/Player.cs
@@ -198,6 +198,7 @@ public class Player : MonoBehaviour {
 	private void showMessage(SMS message) {
 		mobile.SetText (message.Content);
 		mobile.senderName.text = message.Sender;
+        mobile.PlayMessageNotification();
 		mobile.Activate ();
 		cash = cash + message.Transaction;
 	}
